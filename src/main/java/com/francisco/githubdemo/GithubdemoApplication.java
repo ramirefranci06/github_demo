0fd75b86ec42de048f1e4f0511801e01f08b41df
Francisco Ramirez Montoya <1729721@TCS.com>
2020-11-27 12:02:36 -0600

    commiting hello world

--- src/main/java/com/francisco/githubdemo/GithubdemoApplication.java
+++ src/main/java/com/francisco/githubdemo/GithubdemoApplication.java
@@ -8,6 +8,7 @@ public class GithubdemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GithubdemoApplication.class, args);
+		System.out.println("hello world");
 	}
 
 }
