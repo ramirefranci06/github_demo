f7cb912412619047e52ae9ae6a91db1ae2fb8cc6
Francisco Ramirez Montoya <1729721@TCS.com>
2020-11-27 11:54:33 -0600

    first commit

---
+++ src/test/java/com/francisco/githubdemo/GithubdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francisco.githubdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
